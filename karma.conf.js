69997123bd48f9fa460f3c2a2ecef4a5597ddad2
t-kojima <t-kojima@skullware.net>
2018-07-06 16:55:50 +0900

    chore: webpack babel polyfill ...

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,8 @@
 module.exports = config => {
   config.set({
     basePath: '',
-    frameworks: ['mocha', 'chai', 'kintuba'],
+    frameworks: ['mocha', 'chai', 'kintuba'], // 'polyfill'],
+    // polyfill: ['fetch'],
     files: [
       'src/**/*.js',
       'test/**/*.js',
@@ -16,14 +17,41 @@ module.exports = config => {
         nocache: false,
       },
     ],
+    // client: {
+    //   mocha: {
+    //     timeout: 10000,
+    //   },
+    // },
     exclude: [],
-    preprocessors: {},
+    // preprocessors: {
+    //   'test/**/*.js': ['webpack'],
+    // },
+    // webpack: {
+    //   mode: 'development',
+    //   module: {
+    //     rules: [
+    //       {
+    //         test: /\.js$/,
+    //         exclude: /node_modules/,
+    //         use: [
+    //           {
+    //             loader: 'babel-loader',
+    //             options: {
+    //               presets: ['env'],
+    //             },
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
     reporters: ['mocha'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Firefox', 'PhantomJS', 'Edge', 'IE'],
+    // browsers: ['Chrome', 'Firefox', 'PhantomJS', 'Edge', 'IE'],
+    browsers: ['Chrome', 'Firefox', 'Edge'],
     singleRun: true,
     concurrency: Infinity,
   })
